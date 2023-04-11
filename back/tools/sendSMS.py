2d44d1d287067d62b627b997982369ce35a07380
aurdst <74465387+aurdst@users.noreply.github.com>
2023-04-11 11:36:17 +0200

    Update sendSMS.py

--- back/tools/sendSMS.py
+++ back/tools/sendSMS.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Créez un compte Twilio et obtenez votre numéro de compte SID et votre clé d'authentification
-account_sid = "AC10d84daf93f63eab079d7e542214db67"
-auth_token = "4454a73f1c055d52c277a6bde5be4c07"
+account_sid = ""
+auth_token = ""
 
 # Initialisez un client Twilio
 client = Client(account_sid, auth_token)
@@ -12,7 +12,7 @@ message = client.messages \
                 .create(
                      body="Félicitation, ceci est un message de confirmation pour ta participation voici ton code Paypal d'une valeur de 50€ : GDF8MLVX.",
                      from_="NumberTwilio",
-                     to="+33610423765"
+                     to="+"
                  )
 
 # Affichez le résultat
